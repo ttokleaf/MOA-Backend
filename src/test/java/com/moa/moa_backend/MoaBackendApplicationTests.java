4636ef88d4e57e930ea3f5d3b239fedde523644f
hanyong <yong203@naver.com>
2025-09-16 00:04:13 +0900

    init file

---
+++ src/test/java/com/moa/moa_backend/MoaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moa.moa_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
